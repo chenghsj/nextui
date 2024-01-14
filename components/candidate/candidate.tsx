db1c67e316f8ea4eaa7763f764f631a9c281af3c
Cheng <chengjohnsonhs@gmail.com>
2024-01-14 23:12:38 +0800

    test2

--- components/candidate/candidate.tsx
+++ components/candidate/candidate.tsx
@@ -66,7 +66,7 @@ export const Candidate: FC<CandidateProps> = ({ candidate }) => {
   useEffect(() => {
     setCandidate(candidate);
   }, [candidate]);
-  // test
+  // testabcd
   const handleOnPress: ProfilePressEvent =
     (...args) =>
       (e: PressEvent) => {
